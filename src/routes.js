09a0d8e790e36c1cf0cf4e2556d3de5bcef7b531
DiegoRamires <goncalves.ramires@gmail.com>
2019-10-11 18:27:22 -0300

    listing client appointments

--- src/routes.js
+++ src/routes.js
@@ -24,6 +24,7 @@ routes.post('/files', upload.single('file'), FileController.store);
 
 routes.post('/providers', ProviderController.index);
 
+routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 
 export default routes;
