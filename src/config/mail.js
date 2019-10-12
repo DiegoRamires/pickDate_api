9a13c6fe45d9c130b8e293c1cc919f7c63cd77b0
DiegoRamires <goncalves.ramires@gmail.com>
2019-10-12 20:39:27 -0300

    install nodemailer, configure and test service to send an appointment cancel email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '41269e127a0a59',
+    pass: '47bacbdc9c64e9',
+  },
+  default: {
+    from: 'Equipe Barber <noreply@barber.com>',
+  },
+};
